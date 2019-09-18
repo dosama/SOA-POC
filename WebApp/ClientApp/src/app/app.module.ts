5539121aa36d0186273f4357991da13cffb7d7f1
Dina Osama <dosama@integrant.com>
2019-09-19 01:39:55 +0200

    Commit

--- WebApp/ClientApp/src/app/app.module.ts
+++ WebApp/ClientApp/src/app/app.module.ts
@@ -9,6 +9,8 @@ import { NavMenuComponent } from './nav-menu/nav-menu.component';
 import { HomeComponent } from './home/home.component';
 import { CounterComponent } from './counter/counter.component';
 import { FetchDataComponent } from './fetch-data/fetch-data.component';
+import { ServiceBusSender } from './services/service-bus-sender.service';
+import { ServiceBusSubscriber } from './services/service-bus-subscriber.service';
 
 @NgModule({
   declarations: [
--- WebApp/ClientApp/src/app/counter/counter.component.ts
+++ WebApp/ClientApp/src/app/counter/counter.component.ts
@@ -1,13 +1,41 @@
 import { Component } from '@angular/core';
+import { ServiceBusSender } from '../services/service-bus-sender.service';
+import { ServiceBusSubscriber } from '../services/service-bus-subscriber.service';
+import { Payload } from '../models/payload';
 
 @Component({
   selector: 'app-counter-component',
   templateUrl: './counter.component.html'
 })
 export class CounterComponent {
+  
+
+
+  constructor( ) {
+ 
+  }
   public currentCount = 0;
 
   public incrementCounter() {
     this.currentCount++;
+    
+    // const payload: Payload = {
+    //   actionName: 'CounterMessage',
+    //   jsonContent: null
+    // };
+    // this.serviceBusSender.sendMessage(payload)
+   
+
+   
   }
+
+  myMessageHandler = async (payload) => {
+    console.log("service Bus Message Received");
+    console.log(payload);
+  };
+
+   myErrorHandler = (error) => {
+    console.log("service Bus Message Error");
+    console.log(error);
+  };
 }
