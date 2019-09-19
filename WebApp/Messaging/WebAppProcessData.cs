f2774fa0b77d9eb92f6a279d8d64a941efa7f374
Dina Osama <dosama@integrant.com>
2019-09-19 17:03:03 +0200

    Signal R Messages

--- WebApp/Messaging/WebAppProcessData.cs
+++ WebApp/Messaging/WebAppProcessData.cs
@@ -23,25 +23,25 @@ namespace WebApp.Messaging
                 switch (payload.ActionName)
                 {
                     case "GetCourses_Completed":
-                        _hub.Clients.All.SendAsync("transferchartdata", payload);
+                        _hub.Clients.All.SendAsync("GetCourses_Completed", payload);
                         break;
                     case "GetCourseDetails_Completed":
-                        _hub.Clients.All.SendAsync("transferchartdata", payload);
+                        _hub.Clients.All.SendAsync("GetCourseDetails_Completed", payload);
                         break;
                     case "GetExams_Completed":
-                        _hub.Clients.All.SendAsync("transferchartdata", payload);
+                        _hub.Clients.All.SendAsync("GetExams_Completed", payload);
                         break;
                     case "GetExamDetails_Completed":
-                        _hub.Clients.All.SendAsync("transferchartdata", payload);
+                        _hub.Clients.All.SendAsync("GetExamDetails_Completed", payload);
                         break;
                     case "GenerateReport_Completed":
-                        _hub.Clients.All.SendAsync("transferchartdata", payload);
+                        _hub.Clients.All.SendAsync("GenerateReport_Completed", payload);
                         break;
                     case "GetStudents_Completed":
-                        _hub.Clients.All.SendAsync("transferchartdata", payload);
+                        _hub.Clients.All.SendAsync("GetStudents_Completed", payload);
                         break;
                     case "GetStudentDetails_Completed":
-                        _hub.Clients.All.SendAsync("transferchartdata", payload);
+                        _hub.Clients.All.SendAsync("GetStudentDetails_Completed", payload);
                         break;
 
                 }
